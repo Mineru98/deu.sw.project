6056cd59a926c14a6b4af38baca6375766811d76
iamiks <rmstjr1030@naver.com>
2023-05-13 16:34:21 +0900

    관계 코드 추가

--- src/main/java/kr/ac/deu/computer_engineering/Absenteeism/Management/domain/Team/Team.java
+++ src/main/java/kr/ac/deu/computer_engineering/Absenteeism/Management/domain/Team/Team.java
@@ -1,10 +1,14 @@
 package kr.ac.deu.computer_engineering.Absenteeism.Management.domain.Team;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import kr.ac.deu.computer_engineering.Absenteeism.Management.domain.Schedule.Schedule;
+import kr.ac.deu.computer_engineering.Absenteeism.Management.domain.User.User;
 import lombok.*;
 import org.hibernate.Hibernate;
 import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Builder
@@ -23,6 +27,10 @@ public class Team {
     @Comment("부서명")
     @Column(length = 64)
     private String teamName;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+    private List<User> userList;
 
     @Override
     public boolean equals(Object o) {
