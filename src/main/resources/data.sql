1b404ab1fa796b917984994b4b33ee8b60e60ec1
iamiks <rmstjr1030@naver.com>
2023-06-01 23:20:12 +0900

    기본값 입력값

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,27 @@
-SET foreign_key_checks = FALSE;
+SET
+foreign_key_checks = FALSE;
+
 TRUNCATE `Role`;
+INSERT INTO `Role` (`id`, `roleName`, `description`)
+VALUES (1, 'ROLE_CEO', '대표이사'),
+       (2, 'ROLE_STAFF', '직원'),
+       (3, 'ROLE_MANAGER', '부서관리자');
 
-INSERT INTO `Role` (`id`, `roleName`, `description`) VALUES
-(1, 'ROLE_CEO', '대표이사'),
-(2, 'ROLE_STAFF', '직원'),
-(3, 'ROLE_MANAGER', '부서관리자');
+TRUNCATE `Rank`;
+INSERT INTO `Rank` (`id`, `rankName`)
+VALUES (1, '대표이사'),
+       (2, '팀장'),
+       (3, '사원');
 
-SET foreign_key_checks = TRUE;
+TRUNCATE `Company`;
+INSERT INTO `Company` (`id`, `companyName`, `settlementDate`, `address`, `isMain`)
+VALUES (1, '바디메이트(본사)', '2021-01-01', '', 1),
+       (2, '바디메이트(부산지사)', '2021-10-01', '', 0);
+
+TRUNCATE `Team`;
+INSERT INTO `Team` (`id`, `teamName`)
+VALUES (1, '경영팀'),
+       (2, '개발팀');
+
+SET
+foreign_key_checks = TRUE;
